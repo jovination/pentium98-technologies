b3c7952bb45fadc13080872b2c8f0626dade5085
jovination <jovination791@gmail.com>
2024-05-08 15:13:27 +0300

    few changes

--- src/app/components/Herosection/Herosection.jsx
+++ src/app/components/Herosection/Herosection.jsx
@@ -4,6 +4,19 @@ import './Herosection.css'
 function Herosection() {
   return (
     <div className='container'>
+        <div className="section_one">
+            <div className="left_col">
+             <h1 className='h-main'>
+                Where innovation meet <span>Security</span>
+             </h1>
+             <p className="p-main">
+             We work with amazing companies, startups and people in Africa to build things that matter.
+             </p>
+            </div>
+            <div className="right_col">
+
+            </div>
+        </div>
       
     </div>
   )
