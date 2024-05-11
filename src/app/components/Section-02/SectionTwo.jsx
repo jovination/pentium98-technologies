59282ea8f924db6cc2f3a10507208e60205865bd
jovination <jovination791@gmail.com>
2024-05-11 23:24:15 +0300

    working on section-two

---
+++ src/app/components/Section-02/SectionTwo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SectionTwo.css'
+
+function SectionTwo() {
+  return (
+    <div className='section_02'>
+      
+    </div>
+  )
+}
+
+export default SectionTwo
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import   "./page.css";
 import Navbar from "./components/Navbar/Navbar";
 import Herosection from "./components/Herosection/Herosection";
 import SectionOne from "./components/Section-01/SectionOne";
+import SectionTwo from "./components/Section-02/SectionTwo";
     
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
    <Navbar />
    <Herosection />
    <SectionOne />
+   <SectionTwo />
    </div>
   );
 }
