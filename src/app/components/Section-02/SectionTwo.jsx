0091037cd467199adb9f8cc7ba1df79e43adc38f
jovination <jovination791@gmail.com>
2024-05-12 13:19:13 +0300

    working on section-two

--- src/app/components/Section-02/SectionTwo.jsx
+++ src/app/components/Section-02/SectionTwo.jsx
@@ -10,10 +10,17 @@ function SectionTwo() {
             <h1 className='h-section-2'>
                 What we offer
             </h1>
-             <p className='p-section-2-a'>Leveraging our in-depth knowledge of the industry, we are proficient in crafting digital products. We are capable of turning a napkin sketch to a real shipped product that people love.
-
-</p>
-
+             <p className='p-section-2-a'>Leveraging our in-depth knowledge of the industry, we are proficient in crafting digital products. We are capable of turning a napkin sketch to a real shipped product that people love.</p>
+        </div>
+        <div className="lower_2_row">
+          <div className="seg_ui">
+            <div className="seg_ui_wrap"></div>
+            <div className="seg_ui_wrap"></div>
+            <div className="seg_ui_wrap"></div>
+            <div className="seg_ui_wrap"></div>
+            <div className="seg_ui_wrap"></div>
+            <div className="seg_ui_wrap"></div>
+          </div>
         </div>
       
     </div>
