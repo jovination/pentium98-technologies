b509ac934359fd9896c9afa7809ce1541fea631e
jovination <jovination791@gmail.com>
2024-05-14 09:31:48 +0300

    working on section-two

--- src/app/components/Section-02/SectionTwo.jsx
+++ src/app/components/Section-02/SectionTwo.jsx
@@ -14,12 +14,105 @@ function SectionTwo() {
         </div>
         <div className="lower_2_row">
           <div className="seg_ui">
-            <div className="seg_ui_wrap"></div>
-            <div className="seg_ui_wrap"></div>
-            <div className="seg_ui_wrap"></div>
-            <div className="seg_ui_wrap"></div>
-            <div className="seg_ui_wrap"></div>
-            <div className="seg_ui_wrap"></div>
+            
+            <div className="seg_ui_wrap">
+              <div className="img_seg soft_dev">
+
+              </div>
+              <div className="wrapper_seg_ui">
+              <p className='p_title_seg'>
+              Custom Software development
+              </p>
+              </div>
+            </div>
+
+            <div className="seg_ui_wrap">
+            <div className="img_seg mobile_dev">
+                
+                </div>
+            <div className="wrapper_seg_ui">
+            <p className='p_title_seg'>
+            Mobile development
+              </p>
+              </div>
+            </div>
+
+            <div className="seg_ui_wrap">
+            <div className="img_seg embedded_system">
+                
+                </div>
+                <div className="wrapper_seg_ui">
+            <p className='p_title_seg'>
+            Embedded System             </p>
+            </div>
+            </div>
+
+            <div className="seg_ui_wrap">
+            <div className="img_seg soft_main">
+                
+                </div>
+            <div className="wrapper_seg_ui">    
+            <p className='p_title_seg '>
+            Software maintainance
+              </p>
+             </div> 
+            </div>
+            
+            <div className="seg_ui_wrap">
+            <div className="img_seg vul_asses">
+                
+            </div>
+            <div className="wrapper_seg_ui"> 
+            <p className='p_title_seg'>
+            Vulnerabilities Assessment
+              </p>
+            </div>  
+            </div>
+
+            <div className="seg_ui_wrap">
+            <div className="img_seg syst_audit">
+                
+            </div>
+            <div className="wrapper_seg_ui"> 
+            <p className='p_title_seg '>
+            System Auditing
+              </p>
+             </div>
+             </div>
+
+            <div className="seg_ui_wrap">
+            <div className="img_seg penet_test">
+                
+             </div>
+             <div className="wrapper_seg_ui"> 
+                <p className='p_title_seg '>
+                Penetration Testing
+                  </p>
+              </div>    
+            </div>
+
+            <div className="seg_ui_wrap">
+            <div className="img_seg ai_bg_ml">
+                
+                </div>
+             <div className="wrapper_seg_ui">  
+              <p className='p_title_seg'>
+              Ai, Big Data & Machine Learning
+              </p>
+              </div>
+            </div>
+
+            <div className="seg_ui_wrap">
+            <div className="img_seg comp_fore">
+                
+                </div>
+             <div className="wrapper_seg_ui"> 
+              <p className='p_title_seg'>
+              Computer Forensics              
+              </p>
+              </div>
+            </div>
+
           </div>
         </div>
       
